d4bfb5252f43afb264daaba4e425bf76d6dacc2d
nick9956 <37480840+nick9956@users.noreply.github.com>
2024-06-25 15:32:02 +0200

    Finished 8th task

--- HW5_6_7_8.py
+++ HW5_6_7_8.py
@@ -1,4 +1,5 @@
 import csv
+from json import load
 from datetime import datetime
 from random import choice
 from os import path, remove
@@ -10,13 +11,13 @@ class News:
     def __init__(self, text: str, city: str) -> None:
         self.text = text
         self.city = city
-    
+
     def get_news_body(self):
         current_time = datetime.now().strftime('%Y-%m-%d %H:%M')
         return '\nNews -------------------------\n{text}\n{city}, {current_time}\n'.format(
             text=self.text, city=self.city, current_time=current_time)
-    
-    
+
+
 class PrivateAd:
     def __init__(self, text: str, expiration_date: str) -> None:
         self.text = text
@@ -26,8 +27,8 @@ class PrivateAd:
         delta = (self.expiration_date - datetime.now().date()).days
         return '\nPrivate Ad ------------------\n{text}\nActual until: {expiration_date}, {delta} days left\n'.format(
             text=self.text, expiration_date=self.expiration_date, delta=delta)
-    
-    
+
+
 class JokeOfTheDay:
     def __init__(self, text: str) -> None:
         self.text = text
@@ -41,7 +42,7 @@ class JokeOfTheDay:
         funny_meter = self.generate_mark()
         return '\nJoke of the day ------------\n{text}\n{funny_meter}\n'.format(
             text=self.text, funny_meter=funny_meter)
-    
+
 
 class FileReader:
 
@@ -51,9 +52,41 @@ class FileReader:
     def read_file(self):
         records = []
         with open(self.file_path, 'r', encoding='utf8') as file:
-            records = file.readlines()        
+            records = file.readlines()            
         return records
-    
+
+    def delete_read_file(self):
+        remove(self.file_path)
+
+
+class JSONReader:
+
+    def __init__(self, file_path: str = 'data.json'):
+        self.file_path = file_path
+
+    def load_file(self):
+        data = []
+        with open(self.file_path, 'r', encoding='utf8') as file:
+            data = load(file)      
+        return data
+
+    def transform_to_text(self):
+        records = self.load_file()
+        data = ''
+        for record in records:           
+            method = record['method']
+            hyphens = ' -------------------------\n'
+            text = record['text']
+            data += '\n' + method + hyphens + text + '\n'
+            if method == 'News':
+                data += '{city}, {date}\n'.format(city = record['city'], date = record['current_date'])
+            elif method == 'Private ad':
+                data += 'Actual until: {date_until}, {days_left}\n'.format(
+                                    date_until = record['actual_until'], days_left = record['days_left'])
+            elif method == 'Joke of the day':
+                data += record['funny_mark'] + '\n'
+        return data
+
     def delete_read_file(self):
         remove(self.file_path)
 
@@ -65,7 +98,7 @@ class FilePublisher:
 
     def check_file_size(self):
         return path.getsize(self.file_path)
- 
+
     def publish_record(self, data_type):
         with open(self.file_path, 'a') as file:
             file_size = self.check_file_size()
@@ -109,12 +142,24 @@ class FilePublisher:
 
 class FileProcessor:
 
-    def __init__(self, reader_path=None, publisher_path='news_feed.txt'):
+    def __init__(self, reader_path='data.txt', publisher_path='news_feed.txt'):
         self.reader = FileReader(reader_path)
         self.publisher = FilePublisher(publisher_path)
 
     def process_file(self):
         records = self.reader.read_file()
+        self.reader.delete_read_file()
+        self.publisher.publish_records(records)
+
+
+class JSONProcessor:
+
+    def __init__(self, reader_path='data.json', publisher_path='news_feed.txt'):
+        self.reader = JSONReader(reader_path)
+        self.publisher = FilePublisher(publisher_path)
+
+    def process_file(self):
+        records = self.reader.transform_to_text()
         self.reader.delete_read_file()
         self.publisher.publish_records(records)
 
@@ -178,7 +223,7 @@ class SCVGenerator:
 
 
 while True:
-    method = input('------------------------------------------\nHow do you want to add a data? Please choose from the variants below:\n1 - Manually, 2 - From File\nTo exit, type "exit"\n')
+    method = input('------------------------------------------\nHow do you want to add a data? Please choose from the variants below:\n1 - Manually, 2 - From Text File, 3 - From JSON File\nTo exit, type "exit"\n')
 
     if method.lower() == 'exit':
         break
@@ -197,6 +242,18 @@ while True:
         elif location == '2':
             new_location = input('Please enter new file location\n')
             processor = FileProcessor(reader_path=new_location)
+            processor.process_file()
+        else:
+            print('Unknown option was selected. Please try again\n')
+            continue
+    elif method == '3':
+        location = input('Do you want to use default path file location or enter new?\n1 - Default, 2 - New\n')
+        if location == '1':
+            processor = JSONProcessor()
+            processor.process_file()
+        elif location == '2':
+            new_location = input('Please enter new file location\n')
+            processor = JSONProcessor(reader_path=new_location)
             processor.process_file()
         else:
             print('Unknown option was selected. Please try again\n')
